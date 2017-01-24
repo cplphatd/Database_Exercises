ab14220dbc90bd8542c215012f9901f6115875c7
David Ryan Alviola <david.alviola@gmail.com>
2017-01-24 15:45:57 -0600

    Added update_exercises file.

--- src/select_exercises.sql
+++ src/select_exercises.sql
@@ -2,31 +2,37 @@ USE codeup_test_db;
 
 -- Select:
   -- The name of all albums by Pink Floyd.
+  SELECT 'Pink Floyd' AS 'Albums by: ';
   SELECT name
   FROM albums
   WHERE artist = "Pink Floyd";
 
   -- The year Sgt. Pepper's Lonely Hearts Club Band was released
+  SELECT 'Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Year';
   SELECT release_date
   FROM albums
   WHERE name = "Sgt. Peppers Lonely Hearts Club Band";
 
   -- The genre for Nevermind
+  SELECT 'of Nevermind' AS 'Genre';
   SELECT genre
   FROM albums
   WHERE name = "Nevermind";
 
   -- Which albums were released in the 1990s
+  SELECT 'Released in the 90s' AS 'Albums';
   SELECT name
   FROM albums
   WHERE release_date BETWEEN 1990 and 1999;
 
   -- Which albums had less than 20 million certified sales
+  SELECT 'with >$20 million in sales' AS 'Albums';
   SELECT name
   FROM albums
   WHERE sales_in_millions < 20;
 
   -- All the albums in the rock genre. Is this all the rock albums in the table?
+  SELECT 'that are rock' AS 'Albums';
   SELECT name
   FROM albums
   WHERE genre LIKE "%rock%";
---
+++ src/update_exercises.sql
@@ -0,0 +1,32 @@
+USE codeup_test_db;
+
+-- select:
+
+-- All albums in your table.
+  UPDATE albums
+  SET sales_in_millions = sales_in_millions * 10;
+
+  SELECT 'Albums' AS 'All';
+  SELECT * FROM albums\G
+
+-- All albums released before 1980
+  UPDATE albums
+  SET release_date = 1901
+  WHERE release_date < 1980;
+
+  SELECT 'released before 1980' AS 'Albums';
+  SELECT name
+  FROM albums
+  WHERE release_date < 1980;
+
+-- All albums by Peter Jackson
+  UPDATE albums
+  SET artist = 'Peter Jackson'
+  WHERE artist = 'Michael Jackson';
+
+  SELECT 'by Peter Jackson' AS 'Albums';
+  SELECT name
+  FROM albums
+  WHERE artist = 'Peter Jackson'
+
+
